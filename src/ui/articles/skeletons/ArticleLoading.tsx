95d5d0af9fbfd09061e0d03e36d31cc30dc0b2ae
maksym <furmanov.maksym@gmail.com>
2026-04-30 11:53:18 +0200

    "use client" removed from loading

--- src/ui/articles/skeletons/ArticleLoading.tsx
+++ src/ui/articles/skeletons/ArticleLoading.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import styles from "./articles-skeletons.module.css";
 import GalleryLoading from "./GalleryLoading";
 
--- src/ui/articles/skeletons/ArticlesListLoading.tsx
+++ src/ui/articles/skeletons/ArticlesListLoading.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import styles from "./articles-skeletons.module.css";
 import {ArticleLoading} from "./ArticleLoading";
 
